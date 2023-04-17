af043644ee925b1cee9b0cf977e82f0a9fbd7711
Uluc Hosgelen <uluchosgelen@gmail.com>
2023-04-17 16:35:21 +0300

    deneme

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Uluç Hoşgelen on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
